115275fee01872b4a78c247f8594bd6efd75f184
Lihle Mqhayi <2018293130@ufs4life.ac.za>
2023-05-14 22:31:25 +0200

    Added landing page

--- WhatsApp_ChatGPT_Itegration/Controllers/HomeController.cs
+++ WhatsApp_ChatGPT_Itegration/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using WhatsApp_ChatGPT_Itegration.Models;
+
 
 namespace WhatsApp_ChatGPT_Itegration.Controllers
 {
@@ -16,17 +16,6 @@ namespace WhatsApp_ChatGPT_Itegration.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- WhatsApp_ChatGPT_Itegration/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WhatsApp_ChatGPT_Itegration.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
